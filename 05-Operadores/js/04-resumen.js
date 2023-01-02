55fe10a26873839a1144ee59a2318be550717c30
Taiel Alen Nunes <Nuxz55@hotmail.com>
2023-01-02 14:10:58 -0300

    finish section 05

---
+++ 05-Operadores/js/04-resumen.js
@@ -0,0 +1,17 @@
+// Operador mayor a...
+console.log(numero1 > numero3);
+
+// operador menor a...
+console.log(numero1 < numero3);
+
+// comparador igual a...
+console.log(numero1 == numero3);
+
+// comparador estricto
+console.log(numero1 === numero2);
+
+// comparador diferente a...
+console.log(password1 != password2);
+
+// comparador estricto diferente a...
+console.log(numero1 !== numero2);
