620f8ea4263d4cde1a0df39dd6ae3ab23fe1d81a
Taiel Alen Nunes <Nuxz55@hotmail.com>
2023-01-02 10:08:51 -0300

    finish section 01

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,21 @@
+//* consola de javascript *//
+
+// crear e imprimir variable
+const hola = "Hola desde la consola";
+console.log(hola);
+
+// mostrar mensaje com cartel de error
+console.error("Algo salió mal");
+
+// mostrar mensaje com cartel de advertencia
+console.warn("Eso no esta permitido");
+
+// limpiar la consola
+console.clear();
+
+// monitorear el tiempo que tarda algo en ejecutarse
+console.time('Hola'); 
+console.warn("Eso no esta permitido"); 
+console.warn("Eso no esta permitido");
+console.warn("Eso no esta permitido");
+console.timeEnd('Hola');
