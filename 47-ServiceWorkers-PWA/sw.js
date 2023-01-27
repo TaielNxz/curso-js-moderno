7dff3f88ef44d1b27cb3230e4ec0dadc8a2dd420
Taiel Alen Nunes <Nuxz55@hotmail.com>
2023-01-27 10:33:39 -0300

    finish section 47

---
+++ 47-ServiceWorkers-PWA/sw.js
@@ -0,0 +1,52 @@
+const nombreCache = 'apv-v5';
+const archivos = [
+    '/',
+    '/index.html',
+    '/error.html',
+    '/css/bootstrap.css',
+    '/css/styles.css',
+    '/js/app.js',
+    '/js/apv.js'
+];
+
+// Cuando se instala el service Worker
+self.addEventListener('install', e => {
+    console.log('Instalando el Service Worker');
+
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then( cache => {
+                console.log('cacheando');
+                cache.addAll(archivos);
+            })
+    )
+});
+
+// Activar el Service worker
+self.addEventListener('activate', e => {
+    console.log('Service Worker Activado');
+    
+    e.waitUntil(
+        caches.keys()
+            .then( keys => {
+                // console.log(keys)
+
+                return Promise.all(
+                    keys.filter( key => key !== nombreCache )
+                        .map( key => caches.delete(key)) // Borra los demas
+                )
+            })
+    )
+});
+
+// Evento fetch para descargar archivos estaticos
+self.addEventListener('fetch', e => {
+    console.log('Fetch...', e);
+
+    e.respondWith(
+        caches.match(e.request)
+            .then( respuestaCache => {
+                return respuestaCache ?? caches.match('/error.html')
+            })
+    )
+})
